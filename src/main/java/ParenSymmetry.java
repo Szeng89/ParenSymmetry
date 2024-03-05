a929a218e34e417d25076f1a0a8d5e592c54ed99
Szeng89 <shijinz898@gmail.com>
2024-03-05 11:24:43 -0500

    cant figure out read

--- src/main/java/ParenSymmetry.java
+++ src/main/java/ParenSymmetry.java
@@ -1,11 +1,17 @@
 package src.main.java;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class ParenSymmetry {
 
 
     private Boolean isBalanced(String s) {
         int leftParent =0;
         int rightParent =0;
+        
+
         for(int i = 0; i < s.length(); i++){
             if(s.charAt(i) == '('){
                 leftParent++;
@@ -26,7 +32,24 @@ public class ParenSymmetry {
     private void checkFile(String filename) {
         // open file named filename
 
+        File file = new File(filename);
+        Scanner scan;
         // for each line in the file
+
+        try{
+            scan = new Scanner(file);
+            while (scan.hasNextLine()){
+                String str = scan.nextLine();
+                boolean b = isBalanced(str);
+                System.out.println(b);
+            }
+            scan.close();
+
+        }catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+
             // read the line
             // print whether or not the line's parenthesis are balanced
 
